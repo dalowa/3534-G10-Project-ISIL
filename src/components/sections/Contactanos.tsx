e957ca6487f5db21e7b62789e0337205394d6611
dalowa <david.developer.work@gmail.com>
2023-06-28 19:43:23 -0500

    beta

--- src/components/sections/Contactanos.tsx
+++ src/components/sections/Contactanos.tsx
@@ -1,7 +1,33 @@
+import Image from "next/image";
+
 export default function Contactanos() {
   return (
     <>
-      <section className="bg-blue-500 h-screen w-full"></section>
+      <section className="bg-blue-500 w-full p-2 text-white">
+        <div>
+          <h4>Contactanos:</h4>
+          <div className="w-full flex items-center justify-center gap-5">
+            <Image
+              src={
+                "https://raw.githubusercontent.com/dalowa/3534-G10-Project-ISIL/main/public/instagram.png"
+              }
+              alt="instagram"
+              width={250}
+              height={0o0}
+              className="w-1/12"
+            />
+            <Image
+              src={
+                "https://raw.githubusercontent.com/dalowa/3534-G10-Project-ISIL/main/public/facebook.png"
+              }
+              alt="facebook"
+              width={250}
+              height={0o0}
+              className="w-1/12"
+            />
+          </div>
+        </div>
+      </section>
     </>
   );
 }

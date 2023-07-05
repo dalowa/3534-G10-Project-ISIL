19964698880c019be4cfb49cff4423b06b163538
dalowa <david.developer.work@gmail.com>
2023-07-04 22:35:25 -0500

    link

--- src/components/sections/Contactanos.tsx
+++ src/components/sections/Contactanos.tsx
@@ -3,28 +3,26 @@ import Image from "next/image";
 export default function Contactanos() {
   return (
     <>
-      <section className="bg-blue-500 w-full p-2 text-white">
-        <div>
+      <section className="bg-blue-500 w-full p-3 py-5 text-white">
+        <div className="flex items-center">
           <h4 className="text-contactanosText">Contactanos:</h4>
           <div className="w-full flex items-center justify-center gap-5 lg:gap-20">
-            <Image
-              src={
-                "https://raw.githubusercontent.com/dalowa/3534-G10-Project-ISIL/main/public/instagram.png"
-              }
-              alt="instagram"
-              width={250}
-              height={0o0}
-              className="w-1/12 lg:w-20"
-            />
-            <Image
-              src={
-                "https://raw.githubusercontent.com/dalowa/3534-G10-Project-ISIL/main/public/facebook.png"
-              }
-              alt="facebook"
-              width={250}
-              height={0o0}
-              className="w-1/12 lg:w-20"
-            />
+            <a
+              target="_blank"
+              href="https://instagram.com/vecinosmuyunidos?igshid=NGExMmI2YTkyZg== "
+              className="flex gap-5 items-center hover:scale-105 transition"
+            >
+              {"Instagram =>"}
+              <Image
+                src={
+                  "https://raw.githubusercontent.com/dalowa/3534-G10-Project-ISIL/main/public/instagram.png"
+                }
+                alt="instagram"
+                width={250}
+                height={0o0}
+                className="w-1/12 lg:w-20"
+              />
+            </a>
           </div>
         </div>
       </section>
